54c47eafd96761f7ae38daa04daf968be575a8f8
mulark <mulark11@gmail.com>
2022-09-01 20:09:18 -0600

    Fix fast calc

--- src/screen.rs
+++ src/screen.rs
@@ -24,7 +24,7 @@ impl BoundingBox {
     }
 
     pub fn collides_with_circle(&self, pos: Coord, radius: u32) -> bool {
-        if !self.is_close_enough_to_collide(pos, radius as i64) {
+        if !self.collides_fast_calc(pos, radius as i64) {
             return false;
         }
         let (mut testx, mut testy) = (pos.w, pos.h);
@@ -53,11 +53,11 @@ impl BoundingBox {
         false
     }
 
-    pub fn is_close_enough_to_collide(&self, pos: Coord, radius: i64) -> bool {
-        pos.w > self.left_top.w - radius
-            && pos.w < self.right_bottom.w + radius
-            && pos.h > self.left_top.h - radius
-            && pos.h < self.right_bottom.w + radius
+    pub fn collides_fast_calc(&self, pos: Coord, radius: i64) -> bool {
+        pos.w >= self.left_top.w - radius
+            && pos.w <= self.right_bottom.w + radius
+            && pos.h >= self.left_top.h - radius
+            && pos.h <= self.right_bottom.h + radius
     }
 
     pub fn area(&self) -> i64 {
